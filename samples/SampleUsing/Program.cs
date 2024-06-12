d96511d90cf6840488f0ef3518b911175b1ad510
Sergey Olshanskiy <sergey.olshanskiy@gmail.com>
2024-06-12 19:38:54 +0200

    add samples

---
+++ samples/SampleUsing/Program.cs
@@ -0,0 +1,13 @@
+﻿using DateRecurrenceR;
+
+var firstDayOfWeek = Thread.CurrentThread.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
+var beginDate = new DateOnly(2000, 1, 1);
+var endDate = new DateOnly(2000, 1, 31);
+var weekDays = new WeekDays(DayOfWeek.Tuesday, DayOfWeek.Friday);
+// sub range equal recurrence range
+var fromDate = beginDate;
+var toDate = endDate;
+
+var enumerator = Recurrence.GetWeeklyEnumerator(beginDate, endDate, fromDate, toDate, weekDays, firstDayOfWeek);
+
+while (enumerator.MoveNext()) Console.WriteLine($"{enumerator.Current:yyyy-M-d dddd}");
---
+++ samples/UsingWithSubrange/Program.cs
@@ -0,0 +1,14 @@
+﻿using DateRecurrenceR;
+
+const int subRangeYears = 5;
+var beginDate = DateOnly.MinValue;
+var endDate = DateOnly.MaxValue;
+var fromDate = DateOnly.FromDateTime(DateTime.Now);
+var toDate = fromDate.AddYears(subRangeYears);
+
+var enumerator = Recurrence.YearlyByDayOfYear(beginDate, endDate, fromDate, toDate, 256);
+
+Console.WriteLine($"Current day of year is {fromDate.DayOfYear}");
+Console.WriteLine($"Range {beginDate:M/d/yy} - {endDate:M/d/yy}");
+Console.WriteLine($"Sub range {fromDate:M/d/yy} - {toDate:M/d/yy}");
+while (enumerator.MoveNext()) Console.WriteLine($"{enumerator.Current:yyyy-M-d dddd} ({enumerator.Current.DayOfYear})");
