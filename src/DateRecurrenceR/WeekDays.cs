a913e635c8d4695d8767dcdecff1ac6a38c12ffb
Sergey Olshanskiy <sergey.olshanskiy@gmail.com>
2024-06-16 23:52:33 +0200

    some updates

--- src/DateRecurrenceR/WeekDays.cs
+++ src/DateRecurrenceR/WeekDays.cs
@@ -4,31 +4,74 @@ public sealed class WeekDays
 {
     private readonly bool[] _ds = new bool[DaysInWeek];
 
-    public WeekDays(DayOfWeek dayOfWeek)
+    public WeekDays(DayOfWeek day1)
+        : this(day1, day1)
     {
-        MinDay = (DayOfWeek) DaysInWeek;
-
-        _ds[(int) dayOfWeek] = true;
-        UpdateMinDay(dayOfWeek);
     }
 
-    public WeekDays(params DayOfWeek[] daysArray)
+    public WeekDays(DayOfWeek day1, DayOfWeek day2)
+        : this(day1, day2, day2)
+    {
+    }
+
+    public WeekDays(DayOfWeek day1, DayOfWeek day2, DayOfWeek day3)
+        : this(day1, day2, day3, day3)
+    {
+    }
+
+    public WeekDays(DayOfWeek day1, DayOfWeek day2, DayOfWeek day3, DayOfWeek day4)
+        : this(day1, day2, day3, day4, day4)
+    {
+    }
+
+    public WeekDays(DayOfWeek day1, DayOfWeek day2, DayOfWeek day3, DayOfWeek day4, DayOfWeek day5)
+        : this(day1, day2, day3, day4, day5, day5)
+    {
+    }
+
+    public WeekDays(DayOfWeek day1, DayOfWeek day2, DayOfWeek day3, DayOfWeek day4, DayOfWeek day5, DayOfWeek day6)
+        : this(day1, day2, day3, day4, day5, day6, day6)
+    {
+    }
+
+    public WeekDays(DayOfWeek day1,
+        DayOfWeek day2,
+        DayOfWeek day3,
+        DayOfWeek day4,
+        DayOfWeek day5,
+        DayOfWeek day6,
+        DayOfWeek day7)
     {
         MinDay = (DayOfWeek) DaysInWeek;
 
-        for (var i = 0; i < daysArray.Length; i++)
-        {
-            _ds[(int) daysArray[i]] = true;
-            UpdateMinDay(daysArray[i]);
-        }
+        _ds[(int) day1] = true;
+        _ds[(int) day2] = true;
+        _ds[(int) day3] = true;
+        _ds[(int) day4] = true;
+        _ds[(int) day5] = true;
+        _ds[(int) day6] = true;
+        _ds[(int) day7] = true;
+
+        UpdateMinDay(day1, day2, day3, day4, day5, day6, day7);
     }
 
     public DayOfWeek MinDay { get; private set; }
 
     public bool this[DayOfWeek dayOfWeek] => _ds[(int) dayOfWeek];
 
-    private void UpdateMinDay(DayOfWeek dayOfWeek)
+    private void UpdateMinDay(DayOfWeek day1,
+        DayOfWeek day2,
+        DayOfWeek day3,
+        DayOfWeek day4,
+        DayOfWeek day5,
+        DayOfWeek day6,
+        DayOfWeek day7)
     {
-        if (MinDay > dayOfWeek) MinDay = dayOfWeek;
+        MinDay = (DayOfWeek) Math.Min((int) day1,
+            Math.Min((int) day2,
+                Math.Min((int) day3,
+                    Math.Min((int) day4,
+                        Math.Min((int) day5,
+                            Math.Min((int) day6, (int) day7))))));
     }
 }
