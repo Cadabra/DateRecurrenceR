8d7c8b9e830b023685e0bdfe88fa98ab6d6afdb6
Sergey Olshanskiy <sergey.olshanskiy@gmail.com>
2024-07-17 23:53:44 +0200

    add union methods

--- src/DateRecurrenceR/RecurrenceVal.cs
+++ src/DateRecurrenceR/RecurrenceVal.cs
@@ -1,10 +1,21 @@
 using DateRecurrenceR.Collections;
+using DateRecurrenceR.Collections.Val;
 
 namespace DateRecurrenceR;
 
 public readonly partial struct RecurrenceVal
 {
     private static readonly EmptyEnumerator EmptyEnumerator = new();
+
+    public static UnionEnumeratorVal Union(IEnumerator<DateOnly> d1, IEnumerator<DateOnly> d2)
+    {
+        return new UnionEnumeratorVal(new[] {d1, d2});
+    }
+
+    public static UnionEnumeratorVal Union(params IEnumerator<DateOnly>[] enumerators)
+    {
+        return new UnionEnumeratorVal(enumerators);
+    }
 
     private static DateOnly DateOnlyMin(DateOnly val1, DateOnly val2)
     {
