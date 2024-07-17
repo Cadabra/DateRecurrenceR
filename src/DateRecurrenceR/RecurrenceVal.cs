8262a0a3a1c5df182e94518dcbf76d52081c2cb5
Sergey Olshanskiy <sergey.olshanskiy@gmail.com>
2024-07-17 23:49:47 +0200

    add benchmarks

--- src/DateRecurrenceR/RecurrenceVal.cs
+++ src/DateRecurrenceR/RecurrenceVal.cs
@@ -1,4 +1,5 @@
 using DateRecurrenceR.Collections;
+using DateRecurrenceR.Collections.Val;
 
 namespace DateRecurrenceR;
 
@@ -6,6 +7,16 @@ public readonly partial struct RecurrenceVal
 {
     private static readonly EmptyEnumerator EmptyEnumerator = new();
 
+    public static UnionEnumeratorVal Union(IEnumerator<DateOnly> d1, IEnumerator<DateOnly> d2)
+    {
+        return new UnionEnumeratorVal(new [] {d1, d2});
+    }
+
+    public static UnionEnumeratorVal Union(params IEnumerator<DateOnly>[] enumerators)
+    {
+        return new UnionEnumeratorVal(enumerators);
+    }
+    
     private static DateOnly DateOnlyMin(DateOnly val1, DateOnly val2)
     {
         return val1 <= val2 ? val1 : val2;
