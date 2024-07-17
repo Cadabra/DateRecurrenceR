420d3c1ef5f19923fc9add8c5f1e89b91b8abc54
Sergey Olshanskiy <sergey.olshanskiy@gmail.com>
2024-07-17 23:49:05 +0200

    add union

---
+++ src/DateRecurrenceR/Collections/Val/DateEnumerator.Union.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+
+namespace DateRecurrenceR.Collections.Val;
+
+public partial struct UnionEnumeratorVal : IEnumerator<DateOnly>
+{
+    private readonly EWrapper[] _enumerators;
+    private bool _inited = false;
+    private DateOnly? _current = null;
+    
+    internal UnionEnumeratorVal(IReadOnlyList<IEnumerator<DateOnly>> enumerators)
+    {
+        var hash = new HashSet<EWrapper>();
+
+        for (var i = 0; i < enumerators.Count; i++)
+        {
+            if (enumerators[i] is UnionEnumeratorVal ue)
+            {
+                for (var j = 0; j < ue._enumerators.Length; j++)
+                {
+                    hash.Add(ue._enumerators[j]);
+                }
+            }
+            else
+            {
+                hash.Add(new EWrapper(enumerators[i]));
+            }
+        }
+
+        _enumerators = hash.ToArray();
+
+        Current = default;
+    }
+
+    public bool MoveNext()
+    {
+        var nextIndex = -1;
+
+        for (var i = 0; i < _enumerators.Length; i++)
+        {
+            if (_current.HasValue)
+            {
+                while (_enumerators[i].CanMoveNext && _enumerators[i].Enum.Current <= _current.Value)
+                {
+                    _enumerators[i].MoveNext();
+                }
+            }
+            else
+            {
+                _enumerators[i].MoveNext();
+            }
+        }
+
+        // if (!_current.HasValue)
+        {
+            for (var i = 0; i < _enumerators.Length; i++)
+            {
+                if (!_enumerators[i].CanMoveNext) continue;
+
+                _current = _enumerators[i].Enum.Current;
+                nextIndex = i;
+                break;
+            }
+        }
+
+        for (var i = 0; i < _enumerators.Length; i++)
+        {
+            if (!_enumerators[i].CanMoveNext || !(_current > _enumerators[i].Enum.Current)) continue;
+            
+            _current = _enumerators[i].Enum.Current;
+            nextIndex = i;
+        }
+
+        if (nextIndex < 0)
+        {
+            return false;
+        }
+
+        Current = _enumerators[nextIndex].Enum.Current;
+
+        return true;
+    }
+
+    // public bool MoveNext()
+    // {
+    //     throw new NotImplementedException();
+    // }
+
+    public void Reset()
+    {
+        throw new NotSupportedException();
+    }
+
+    public DateOnly Current { get; private set; }
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+    }
+    
+    private struct EWrapper
+    {
+        public EWrapper(IEnumerator<DateOnly> @enum)
+        {
+            Enum = @enum;
+            CanMoveNext = true;
+        }
+
+        public IEnumerator<DateOnly> Enum { get; }
+        public bool CanMoveNext { get; private set; }
+
+        public bool MoveNext()
+        {
+            CanMoveNext = Enum.MoveNext();
+            return CanMoveNext;
+        }
+
+        public override int GetHashCode()
+        {
+            return Enum.GetHashCode();
+        }
+    }
+}
